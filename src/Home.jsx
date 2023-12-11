655ed2d8f1acfb97fa2ab3d843f1350e92436c10
Liz Nogueira <nogueiraliz00@gmail.com>
2023-12-11 00:27:31 -0300

    styles

--- src/Home.jsx
+++ src/Home.jsx
@@ -9,7 +9,7 @@ function Home() {
       <div className='container'>
         <div>
             <p className='welcome'>BEM VINDX!</p>
-            <p >Esse projeto foi realizado pelo G3E com o intuito de ajudar nós meros estudantes.</p>
+            <p>Esse projeto foi realizado pelo G3E com o intuito de ajudar nós meros estudantes.</p>
         </div>
         <div className='buttons'>
             <button><Link to="/nsg">Cálculo de NSG</Link></button>
