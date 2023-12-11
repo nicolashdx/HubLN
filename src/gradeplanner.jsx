97aa66d42774b34164e515e472b59e0cb20c6a7e
Nicolas Henrique <nicolas.hdx8@gmail.com>
2023-12-10 22:32:12 -0300

    criado pag de grade

---
+++ src/gradeplanner.jsx
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+
+function GradePlanner() {
+  return (
+    <>
+      <div className='container'>
+        <p>Montar Grade</p>
+      </div>
+    </>
+  )
+}
+
+export default GradePlanner
