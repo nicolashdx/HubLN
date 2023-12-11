08727c14a4cee6f208e865fa5cf5ef31b4e9ca07
Nicolas Henrique <nicolas.hdx8@gmail.com>
2023-12-11 13:24:13 -0300

    home quase pronta ate o nicolas acabar com minha felicxidade

--- src/Home.jsx
+++ src/Home.jsx
@@ -11,6 +11,17 @@ function Home() {
         <div>
             <p className='welcome'>BEM <br></br> VINDE!</p>
             <p className= 'textintro'>Esse projeto foi realizado pelo G3E com o intuito de ajudar nós meros estudantes.(texto a se discutir)</p>
+            <div className='links'>
+              <a href="https://www.instagram.com/zeus.g3e/" target="_blank">
+                <img src="src\assets\instagram.svg" alt="Perfil do G3E no Instagram" />
+              </a>
+              <a href="https://drive.google.com/drive/folders/0Bza5WseGdGvvOW82ckN2TEtGeFk?resourcekey=0-5L4NShYSxGIQ2zYH6WtPFA&usp=sharing" target="_blank">
+                <img src="src\assets\gdrive.svg" alt="Perfil do G3E no Instagram" />
+              </a>
+              <a href="https://chat.whatsapp.com/Fi8Pw4T3n2eFI38pUBccPD" target="_blank">
+                <img src="src\assets\chat.svg" alt="Grupo Geral de EE no Whatsapp" />
+              </a>
+            </div>
         </div>
         <div className='buttons'>
             <Link to="/nsg"><button>Cálculo de NSG</button></Link>
