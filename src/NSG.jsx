f2747c40fa9ffb492362867bdfedae231ff4e076
Nicolas Henrique <78608431+nicolashdx@users.noreply.github.com>
2023-12-22 15:21:29 +0000

    nsg page

--- src/NSG.jsx
+++ src/NSG.jsx
@@ -12,11 +12,19 @@ function NSG() {
   const [quant_discp, set_quant_discp] = useState(1)
   
   const add_discp = () => {
+    const nota_value = document.getElementsByClassName('nota_input')[0].value
+    if (!nota_value || nota_value < 0 || nota_value > 100){
+      console.log("Valor de nota inválido.")
+      return;
+    }
+     
     const ch = document.createElement('p');
-    ch.textContent = `${document.getElementsByClassName('ch')[0].value}H`
+    ch.textContent = `${document.getElementsByClassName('ch_input')[0].value}H`;
+    ch.className = 'ch'
 
     const nota = document.createElement('p');
-    nota.textContent = `${document.getElementsByClassName('nota')[0].value}`
+    nota.textContent = `${nota_value}`;
+    nota.className = 'nota';
 
     const btn = document.createElement('button');
     btn.innerText = "X"
@@ -31,8 +39,8 @@ function NSG() {
     var list = document.getElementsByClassName('listDiscp')[0];
     list.appendChild(item);
 
-    document.getElementsByClassName('ch')[0].value = ''
-    document.getElementsByClassName('nota')[0].value = ''
+    document.getElementsByClassName('ch_input')[0].value = '30'
+    document.getElementsByClassName('nota_input')[0].value = ''
     
     set_quant_discp(quant_discp + 1);
   }
@@ -45,9 +53,14 @@ function NSG() {
           <p className='titulo'>Cálculo de NSG</p>
           <div className='indiv'>
             <p>Carga Horária:</p>
-            <input className='ch' type="number" placeholder='Ex: 60'/>
+            <select className='ch_input'>
+              <option value="30">30 H</option>
+              <option value="45">45 H</option>
+              <option value="60">60 H</option>
+              <option value="90">90 H</option>
+            </select>
             <p>Nota:</p>
-            <input className='nota' type="number" placeholder='Ex: 75'/>
+            <input className='nota_input' type="number" placeholder='Ex: 75'/>
             <button onClick={add_discp}>+</button>
           </div>
           <div>
