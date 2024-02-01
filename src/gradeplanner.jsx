bb381c4dcb6ce7bf2d818d95f6851ba227a68c5f
Liz Nogueira <nogueiraliz00@gmail.com>
2024-01-31 22:00:41 -0300

    styled input pt1

--- src/gradeplanner.jsx
+++ src/gradeplanner.jsx
@@ -6,14 +6,19 @@ import { Link } from 'react-router-dom';
 
 import './gradeplanner.css'
 
+
 function GradePlanner() {
   return (
     <>
       <div className='retangulo_in'></div>
       <div className='container'>
-        <p>Montar Grade</p>
-        <Link to="/test"><p>Teste</p></Link>
-        <></>
+        <div className= 'field'>
+        <input
+         type="text"
+         placeholder="materias"/>
+
+          </div>
+          
         <Navbar />
       </div>
     </>
