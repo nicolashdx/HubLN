7ba8bf43ee37b6835597a836c61f44c669ce0ecd
Nicolas Henrique <nicolas.hdx8@gmail.com>
2024-02-03 00:15:34 -0300

    build

--- src/home.jsx
+++ src/home.jsx
@@ -30,8 +30,8 @@ function Home({setNav}) {
         </div>
         <div className='buttons'>
             <Link to="/nsg"><button>Cálculo de NSG</button></Link>
-            <Link to="/grade"><button>Montar Grade</button></Link>
-            <Link to="/test"><button>Em breve</button></Link>
+            <Link to="/"><button>Montar Grade</button></Link>
+            <Link to="/"><button>Em breve</button></Link>
         </div>
       </div>
     </>
